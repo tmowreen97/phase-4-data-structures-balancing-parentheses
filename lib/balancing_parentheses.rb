2a7fed3f46f2da5b9a9759fefec0ff4e1c7bd072
Tasreen Mowreen <tmowreen97@gmail.com>
2023-02-26 18:13:12 -0500

    done

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,23 @@
 require_relative './stack'
+require 'pry'
 
 # your code here
+
+def balancing_parentheses input
+arr_input = input.split('')
+  open_parentheses = Stack.new
+  close_parentheses = []
+  if arr_input.first == ')' and arr_input.last =='('
+    return 2
+  else
+    arr_input.each do |value|
+      if value == '('
+        open_parentheses.push(value)
+      else 
+        close_parentheses.push(value)
+      end
+    end
+  end
+  open_parentheses.size == close_parentheses.length ? 0 : (open_parentheses.size-close_parentheses.length).abs()
+
+end
